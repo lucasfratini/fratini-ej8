4a8509ca3da07c649627e8518daf8fc8239cbcb7
lucasfratini <matias_anubis@hotmail.com>
2024-11-12 16:01:01 -0300

    Agregar flujo de trabajo para pruebas unitarias3

--- .github/test.py
+++ .github/test.py
@@ -1,8 +1,8 @@
 import unittest
 
-class TestExample(unittest.TestCase):
-    def test_addition(self):
-        self.assertEqual(1 + 1, 2)
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum([1, 2]), 3)
 
 if __name__ == '__main__':
     unittest.main()
